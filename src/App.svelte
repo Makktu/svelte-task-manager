dcd7da54c7caaef6c7393b59493e8d647ab300ed
Makktu <johnmcna@gmail.com>
2023-11-19 01:45:09 +0000

    progressing

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,39 @@
 <script>
 	import ToDoInputForm from "./ToDoInputForm.svelte";
+  import ToDoList from "./ToDoList.svelte";
 
-  let firstName = 'Jakk';
-  let lastName = 'Makk';
+  let nameEntered = false;
+
+  let firstName = '';
+  let lastName = '';
 
   $: fullName = firstName + ' ' + lastName;
+
+  const handleSubmit = () => {
+    if (firstName && lastName) {
+      nameEntered = true;
+    }
+  }
 </script>
 
 <main>
-	<ToDoInputForm userName={fullName}/>
+  {#if nameEntered}
+    <h1>Welcome, {fullName}</h1>
+    <ToDoInputForm userName={fullName}/>
+    <ToDoList/>
+  {:else}
+    <h1>Welcome to task manager!</h1>
+    <h3>Please enter your name:</h3>
+    <form on:submit|preventDefault={handleSubmit}>
+      <div>
+        <input bind:value={firstName} type="text" placeholder="First name" required>
+      </div>
+      <div>
+        <input bind:value={lastName} type="text" placeholder="Last name" required>
+      </div>
+      <button on:click={handleSubmit}>Start Managing Tasks</button>
+    </form>    
+  {/if}
 </main>
 
 <style>
@@ -17,6 +42,7 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+    background-color: aquamarine;
 	}
 
 	h1 {
---
+++ src/ToDoList.svelte
@@ -0,0 +1,30 @@
+<script>
+  let toDoItems = [
+                    {text: 'Take out trash', status: true},
+                    {text: 'Study programming', status: false},
+                    {text: 'Text Lucy', status: false}
+                  ];
+
+  function removeFromList(i) {
+    toDoItems.splice(i, 1)
+    toDoItems = toDoItems
+  }
+</script>
+
+{#each toDoItems as item, index}
+  <div class="toDoItems">
+    <input bind:checked={item.status} type="checkbox">
+    <span class:checked={item.status}>{item.text}</span>
+    <button on:click={() => removeFromList(index)}>Delete</button>
+    <br/>
+  </div>
+{/each} 
+
+<style>
+  .checked {
+      text-decoration: line-through;
+  }
+  .toDoItems {
+      text-align: center;
+  }
+</style>
